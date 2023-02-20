04aace02018cc60fe27dd2652cdf6d3ec855caeb
mary3210 <maryrodri3210@gmail.com>
2023-02-20 17:37:13 -0600

    pseudo code

--- validate-sub.js
+++ validate-sub.js
@@ -1,6 +1,21 @@
+/* first iterate through the array
+check if number in sequence is in array
+if number is not in sequence return false
+if number is in sequence check next number
+check the index of the first number compared to the second number 
+if the index comes before the index of the second number return true and check next number
+if not return false
+
+
+*/
+
 function isValidSubsequence(array, sequence) {
     // Write your code here.
+
   }
   
+array = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
   // Do not edit the line below.
   exports.isValidSubsequence = isValidSubsequence;
